ed5912591f7aedbc93075ca161c6d05e2b1c9980
GorgeousSuit <gorgeoussuit573@gmail.com>
2025-03-27 16:49:29 +0200

    Description Test

--- components/Description.jsx
+++ components/Description.jsx
@@ -19,7 +19,7 @@ const Description = () => {
     }, []);
 
     const isInView = useInView(ref, {
-        margin: mobileView ? '50% 0px 50% 0px' : '-20% 0px -20% 0px'
+        margin: mobileView ? '-20% 0px -20% 0px' : '-20% 0px -20% 0px'
     });
 
     const scrollToContact = () => {
@@ -29,27 +29,31 @@ const Description = () => {
     };
 
     return (
-        <section className="w-[100%] xl:h-[550px] pb-[60px] relative flex max-lg:flex-col justify-between px-[12px] sm:px-[24px] lg:px-[32px]">
-            <motion.div
-                ref={ref}
-                initial={{ opacity: 0, x: -50 }}
-                animate={isInView && { opacity: 1, x: 0 }}
-                transition={{
-                    duration: 1.5,
-                    ease: [0.22, 1, 0.36, 1]
-                }}
-                viewport={{ once: true }}
-            >
-                <Image
-                    src="/images/About/Banner2.jpg"
-                    alt="Image"
-                    width={450}
-                    height={350}
-                    className="w-72 h-56 md:w-[450px] md:h-[350px] rounded-r-3xl object-cover border border-primary max-lg:object-center lg:object-[-200px_center] xl:object-[right_center]"
-                    unoptimized
-                />
-            </motion.div>
-            <div className="text-center flex flex-col items-center space-y-5 max-sm:mb-[35px] max-sm:mt-[15px] my-[0px] sm:my-[40px] px-[20px]">
+        <section className="w-[100%] pb-[60px] relative flex max-lg:flex-col justify-center px-[12px] sm:px-[24px] lg:px-[32px]">
+            
+            <div className=" absolute top-0 left-[50%] z-[-1] mr-[12px] sm:mr-[24px] lg:mr-[32px] overflow-hidden flex flex-col">
+                <motion.div
+                    ref={ref}
+                    initial={{ x: -20 }}
+                    animate={isInView && {x: 0 }}
+                    transition={{
+                        duration: 2,
+                        ease: [0.22, 1, 0.36, 1]
+                    }}
+                    viewport={{ once: true }}
+                    className="brightness-[70%]"
+                >
+                    <Image
+                        src="/images/About/Banner2.jpg"
+                        alt="Image"
+                        width={450}
+                        height={350}
+                        className="w-full max-w-[500px] md:w-full h-[350px] rounded-r-3xl object-cover border border-primary object-center"
+                        unoptimized
+                    />
+                </motion.div>
+            </div>
+            <div className="max-w-[950px] text-center flex flex-col items-center space-y-5 max-sm:mb-[35px] max-sm:mt-[15px] my-[0px] sm:my-[40px] px-[20px] text-white">
                 <motion.div className="relative overflow-hidden flex flex-col">
                     <motion.div
                         ref={ref}
@@ -125,26 +129,29 @@ const Description = () => {
                     </motion.div>
                 </div>
             </div>
-            <motion.div
-                ref={ref}
-                initial={{ opacity: 0, x: 50 }}
-                animate={isInView && { opacity: 1, x: 0 }}
-                transition={{
-                    duration: 1.5,
-                    ease: [0.22, 1, 0.36, 1]
-                }}
-                viewport={{ once: true }}
-                className="place-self-end"
-            >
-                <Image
-                    src="/images/About/IMG_0861.jpg"
-                    alt="Image"
-                    width={450}
-                    height={350}
-                    className="w-72 h-56 md:w-[450px] md:h-[350px] rounded-l-3xl object-cover border border-primary object-center lg:object-[-100px_center]  xl:object-center"
-                    unoptimized
-                />
-            </motion.div>
+            
+            <div className="absolute top-0 right-[50%] z-[-1] ml-[12px] sm:ml-[24px] lg:ml-[32px] overflow-hidden flex flex-col">
+                <motion.div
+                    ref={ref}
+                    initial={{ x: 20 }}
+                    animate={isInView && {x: 0 }}
+                    transition={{
+                        duration: 2,
+                        ease: [0.22, 1, 0.36, 1]
+                    }}
+                    viewport={{ once: true }}
+                    className="brightness-[65%]"
+                >
+                    <Image
+                        src="/images/About/IMG_0861.jpg"
+                        alt="Image"
+                        width={450}
+                        height={350}
+                        className="w-full max-w-[500px] md:w-full h-[350px] rounded-l-3xl object-cover border border-primary object-center"
+                        unoptimized
+                    />
+                </motion.div>
+            </div>
         </section>
     );
 };
