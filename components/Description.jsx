86da671ef2f983d5a81885ef2519ed135f2a9adb
GorgeousSuit <gorgeoussuit573@gmail.com>
2025-03-31 17:40:59 +0300

    Fixed Description

--- components/Description.jsx
+++ components/Description.jsx
@@ -34,7 +34,7 @@ const Description = () => {
             <div className="absolute top-0 left-[50%] z-[-1] mr-[12px] sm:mr-[24px] lg:mr-[32px] overflow-hidden flex flex-col">
                 <motion.div
                     ref={ref}
-                    initial={window.innerWidth <= 1024 ? { x: "-47vw" } : { x: -490 }}
+                    initial={window.innerWidth <= 1024 ? { x: "-48vw" } : { x: -500 }}
                     animate={isInView && {x: 0 }}
                     transition={{
                         duration: 2,
@@ -53,8 +53,9 @@ const Description = () => {
                     />
                 </motion.div>
             </div>
+
             <div className="max-w-[950px] text-center flex flex-col items-center space-y-5 max-sm:mb-[35px] max-sm:mt-[15px] my-[0px] sm:my-[40px] px-[20px] text-white">
-                <motion.div className="relative overflow-hidden flex flex-col">
+                <motion.div className="relative overflow-hidden flex flex-col ">
                     <motion.div
                         ref={ref}
                         initial={{ y: 65 }}
@@ -68,7 +69,7 @@ const Description = () => {
                         className="relative overflow-hidden flex flex-col"
                     >
                         <Image
-                            src="/logo/Main black logo.png"
+                            src="/logo/Sec. white logo.png"
                             alt="Logo"
                             width={120}
                             height={120}
@@ -133,7 +134,7 @@ const Description = () => {
             <div className="absolute top-0 right-[50%] z-[-1] ml-[12px] sm:ml-[24px] lg:ml-[32px] overflow-hidden flex flex-col">
                 <motion.div
                     ref={ref}
-                    initial={window.innerWidth <= 1024 ? { x: "47vw" } : { x: 490 }}
+                    initial={window.innerWidth <= 1024 ? { x: "48vw" } : { x: 500 }}
                     animate={isInView && {x: 0 }}
                     transition={{
                         duration: 2,
